14cc987ab6b2b9f86c661a99c85bb685bf8c495f
Sabani, Arben <Arben.Sabani@lionbridge.com>
2019-03-24 15:10:46 +0100

    Add project files.

---
+++ ffhs_nmt2_translator_ui/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ffhs_nmt2_translator_ui.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ffhs_nmt2_translator_ui/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ffhs_nmt2_translator_ui
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
